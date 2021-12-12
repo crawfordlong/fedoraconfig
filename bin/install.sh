7c1e78654dcb8d6aa48633bf18b0879274a2175b
Crawford Long <cbl@crawfordlong.com>
2021-12-12 12:52:14 -0500

    Add initial install steps

---
+++ bin/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Script: install.sh
+#
+# Purpose: A simple shell script to initialize a new Fedora installation.
+#
+# Author: Crawford Long
+#
+
+# Set default scope
+if [[ -z "$SCOPE" ]]; then 
+  echo "You haven't set a SCOPE so the SCOPE has been set to 'personal'."
+  SCOPE="personal"
+fi
+
+# Allowed scopes
+if [[ "$SCOPE" != "personal" && "$SCOPE" != "demo" ]]; then
+  echo "Your SCOPE must be set to either 'personal' or 'demo'. Exiting with error 'INVALID SCOPE'."
+  exit 1
+fi
+
+echo "** INSTALL: Core Packages **"
+dnf install --idempotent --allow-inactive --apply-live ansible neovim zsh flatpak-builder git python4 python3-psutil stow wl-clipboard ripgrep git-crypt alacritty
